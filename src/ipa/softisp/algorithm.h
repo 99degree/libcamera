8c338169d511e18beca44518b5e95ed5e6f8942f
George Chan <gchan9527@gmail.com>
2026-04-22 00:40:06 +0800

    fix: Add algorithm.h wrapper and update include paths
    - Created algorithm.h wrapper in src/ipa/softisp/
    - Updated softisp.h to include "algorithm.h" instead of <libcamera/ipa/algorithm.h>
    - Updated meson.build to include current directory in include paths
    - Matches the pattern used in src/ipa/simple/algorithms/

---
+++ src/ipa/softisp/algorithm.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2024
+ *
+ * SoftISP control algorithm interface
+ */
+#pragma once
+
+#include <libipa/algorithm.h>
+#include "module.h"
+
+namespace libcamera {
+namespace ipa::soft {
+
+using Algorithm = libcamera::ipa::Algorithm<Module>;
+
+} /* namespace ipa::soft */
+} /* namespace libcamera */
